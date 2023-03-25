dbf54accaf8af10cb9b5bf5e096f1242fdea9810
Alan Snyder <alan@deronyan.com>
2023-03-25 18:11:48 -0400

    Better output, log failed blocks

--- eth_get_blocks.sh
+++ eth_get_blocks.sh
@@ -1,21 +1,38 @@
 # get ethereum blocks
-ROOT=/media/alan/data/columbo-data/blocks/ethereum/mainnet
+#set -x
+ROOT=/mnt/data/columbo-data/blocks/ethereum/mainnet
+count=0
+dup=0
+ok=0
+fail=0
+total=0
 for n in `seq 1660000 16700000`; do
     h=`printf "%x" $n`;
     FILE=$ROOT/block$n.json
     if [ -f "$FILE" ]; then
-        echo -n "."
+        echo -n "_"
+	dup=$((dup+1))
     else
-        echo "Getting $FILE..."
-        curl https://distinguished-sly-liquid.discover.quiknode.pro/7684eee5885d6f6e7b8e3f88c60004facf139208/ \
-         -X POST \
-         --silent \
-         -H "Content-Type: application/json" \
-         --data "{\"method\":\"eth_getBlockByNumber\",\"params\":[\"0x$h\",true],\"id\":$n,\"jsonrpc\":\"2.0\"}" \
-         -o $FILE
+        curl https://distinguished-sly-liquid.discover.quiknode.pro/7684eee5885d6f6e7b8e3f88c60004facf139208a/ \
+             -X POST \
+             --silent \
+             -H "Content-Type: application/json" \
+             --data "{\"method\":\"eth_getBlockByNumber\",\"params\":[\"0x$h\",true],\"id\":$n,\"jsonrpc\":\"2.0\"}" \
+             -o $FILE
 	if [ $? -ne 0 ]; then
 	    echo "$n" >> ./failed_blocks.out
+	    echo -n "x"
+	    fail=$((fail+1))
+	else
+	    echo -n "+"
+	    ok=$((ok+1))
 	fi
-	sleep 2;
+	sleep 1;
+    fi
+    count=$((count+1))
+    total=$((total+1))
+    if [ $count -eq 120 ]; then
+	echo "  $dup Dup, $ok Ok, $fail Fail, $total Total"
+	count=0
     fi
 done
